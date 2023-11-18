ecf333c9e50be0c725cf907b78008010c55b1dc9
JeremyS-T <jtanrev@gmail.com>
2023-11-18 11:51:45 +0800

    Webpage parse

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,31 @@
-const article = document.querySelector("article");
+function getTextNodes(element) {
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
+  let textNodes = [];
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
+  function traverse(element) {
+      if (element.nodeType === 3) {
+          let obtained_val = element.nodeValue.trim();
+          if (obtained_val != "") {
+              textNodes.push(obtained_val);
+          }
+          if (obtained_val.includes(" ")) {
+              let f_val = obtained_val.split(/\s+/);
 
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+              for (var i = 0; i < f_val.length; i++) {
+                  textNodes.push(f_val[i]);
+              }
+          }
+      } else {
+          for (let i = 0; i < element.childNodes.length; i++) {
+              traverse(element.childNodes[i]);
+          }
+      }
+  }
+
+  traverse(element);
+  return textNodes;
 }
+
+const rootElement = document.body;
+
+const textNodesArray = getTextNodes(rootElement);
