a770ef73e45bbc9c50a8c1480f5f9be1ca7334cb
JeremyS-T <jtanrev@gmail.com>
2023-11-18 12:33:26 +0800

    Add ability to remove special characters in array items.

--- scripts/content.js
+++ scripts/content.js
@@ -5,15 +5,16 @@ function getTextNodes(element) {
   function traverse(element) {
       if (element.nodeType === 3) {
           let obtained_val = element.nodeValue.trim();
-          const regex = /[.,'(){}[\]<>?!@#$%^&*;:]/g;
+          const regex = /[.,'(){}[\]<>?!@#\$%""^/&*;:]/g;
           let f_obtained = obtained_val.replace(regex, '');
 
           if (f_obtained.includes(" ")) {
+            if (f_obtained != " " || f_obtained != "") {
               let f_val = f_obtained.split(" ");
-
               for (var i = 0; i < f_val.length; i++) {
                   textNodes.push(f_val[i]);
               }
+            }
           }
 
           else if (f_obtained != "") {
@@ -31,11 +32,6 @@ function getTextNodes(element) {
   return textNodes;
 }
 
-// Get the root element of the DOM (e.g., the body element)
 const rootElement = document.body;
 
-// Get all regular text nodes from the root element
 const textNodesArray = getTextNodes(rootElement);
-
-// Display the array of text nodes
-console.log(textNodesArray);
