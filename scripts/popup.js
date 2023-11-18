86231156efb8bb39269eb48609da869d36f41d3e
Chester Corpuz <chezramosc@gmail.com>
2023-11-17 18:28:39 -1000

    Added toggle functionality for the button

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,18 @@
-console.log('This is a popup!');
+//power on or off the event
+function enable(event) {
+    console.log('clicked', event)
+    tog = tog ? false : true;
+    if(tog){
+     //turn on...
+        chrome.browserAction.setIcon({ path: 'start-button.png' });
+        chrome.browserAction.setBadgeText({ text: 'ON' });
+        chrome.tabs.executeScript(null, { file: 'content.js' }); 
+    }else{
+     //turn off...
+        chrome.browserAction.setIcon({ path: 'disable.png'});
+        chrome.browserAction.setBadgeText({ text: '' });
+    }
+};
+
+const btn = document.getElementById('press-button');
+btn.addEventListener('click', enable);
