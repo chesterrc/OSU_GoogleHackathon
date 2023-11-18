64c30518c11c377139afc8954779deab7313b398
JeremyS-T <jtanrev@gmail.com>
2023-11-18 12:15:57 +0800

    Refine parse logic

--- scripts/content.js
+++ scripts/content.js
@@ -5,16 +5,21 @@ function getTextNodes(element) {
   function traverse(element) {
       if (element.nodeType === 3) {
           let obtained_val = element.nodeValue.trim();
-          if (obtained_val != "") {
-              textNodes.push(obtained_val);
-          }
-          if (obtained_val.includes(" ")) {
-              let f_val = obtained_val.split(/\s+/);
+          const regex = /[.,'(){}[\]<>?!@#$%^&*;:]/g;
+          let f_obtained = obtained_val.replace(regex, '');
+
+          if (f_obtained.includes(" ")) {
+              let f_val = f_obtained.split(" ");
 
               for (var i = 0; i < f_val.length; i++) {
                   textNodes.push(f_val[i]);
               }
           }
+
+          else if (f_obtained != "") {
+              textNodes.push(f_obtained);
+          }
+          
       } else {
           for (let i = 0; i < element.childNodes.length; i++) {
               traverse(element.childNodes[i]);
@@ -26,6 +31,11 @@ function getTextNodes(element) {
   return textNodes;
 }
 
+// Get the root element of the DOM (e.g., the body element)
 const rootElement = document.body;
 
+// Get all regular text nodes from the root element
 const textNodesArray = getTextNodes(rootElement);
+
+// Display the array of text nodes
+console.log(textNodesArray);
