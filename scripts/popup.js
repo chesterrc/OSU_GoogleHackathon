83f5c493a05f848a562418068213c065c9d4530a
Chester Corpuz <chezramosc@gmail.com>
2023-11-19 08:43:02 -1000

    Added functionality to pause and start, changing images

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,16 +1,46 @@
-//power on or off the event
-function enable(event) {
-    var id = chrome.runtime.id;
-    chrome.management.get(id, function(ex)
-    {
-        if(ex.enabled){
-            chrome.
-            chrome.management.setEnabled(id, false);
-        } else{
-            chrome.management.setEnabled(id, true);
+//get tabid
+var activeTabId;
+//this does not work
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+    activeTabId = activeInfo.tabId;
+});
+
+function getActiveTab(callback) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var tab = tabs[0];
+
+    if (tab) {
+        callback(tab);
+    } else {
+        chrome.tabs.get(activeTabId, function (tab) {
+        if (tab) {
+            callback(tab);
+        } else {
+            console.log('No active tab identified.');
+        }
+    });
+
+    }
+});
+}
+
+//power on
+function poweron() {
+    chrome.storage.sync.get('state', function(data) {
+        if (data.state === 'on') {
+            chrome.storage.sync.set({state: 'off'});
+            //change img src
+            document.getElementById('onoff_img').src = '../images/start-button.png'
+        } else {
+            chrome.storage.sync.set({state: 'on'});
+            document.getElementById('onoff_img').src = '../images/pause.png'
+            var query = { active: true, currentWindow: true };
+            chrome.scripting.executeScript({
+                target: activeTabId,
+                files: ['../backend/html_parse.js'],
+            })
         }
     });
 }
 
-const btn = document.getElementById('press-button');
-btn.addEventListener('click', enable);
+document.getElementById('onoff').addEventListener('click', poweron);
