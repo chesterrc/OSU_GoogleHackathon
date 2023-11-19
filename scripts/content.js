bcc0c3ab82a0b76f1dcabcc5c1b8f28de1531c53
JeremyS-T <jtanrev@gmail.com>
2023-11-20 06:25:18 +0800

    Add ability to censor italicized text and still retain link redirect ability.

--- scripts/content.js
+++ scripts/content.js
@@ -199,22 +199,23 @@ async function main() {
         }
     }
 
-    const lists = document.body.getElementsByTagName("li");
-
-    if (lists.length) {
-        for (const list of lists) {
-            list.childNodes.forEach(item => {
-                // Check if the node is an element node and not a <style> element
-                if (item.tagName.toLowerCase != "style") {
-                    const result = wordGenerator(item.textContent, wordBank);
-                    item.textContent = result.censoredText;
-                    profanityCount += result.profanityCount;
-                }
-            });
+    const arrayOfElementsSpec = ["cite", "li", "i"];
+    for (let i = 0; i < arrayOfElementsSpec.length; i++) {
+        const lists = document.body.getElementsByTagName(arrayOfElementsSpec[i]);
+        if (lists.length) {
+            for (const list of lists) {
+                list.childNodes.forEach(item => {
+                    if (item != undefined) {
+                        if (item.tagName.toLowerCase != "style") {
+                            const result = wordGenerator(item.textContent, wordBank);
+                            item.textContent = result.censoredText;
+                            profanityCount += result.profanityCount;
+                        }
+                    }
+                });
+            }
         }
     }
-
-
     
 
     console.timeEnd("Exec Time");
